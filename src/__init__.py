a425689dc5933aa1bfb929a9ebe805d5c17010fe
kimianili <151434963+kimianili@users.noreply.github.com>
2025-10-21 15:06:40 -0700

    initial structure with src and tests folders

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""
+This file makes the `src/` folder a Python package. It can optionally expose
+top-level metadata (e.g. __version__) or perform lightweight package-level
+imports for convenience.
+
+"""
